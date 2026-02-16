15db1a67216b14da4fbbb2379b02260ee9e28805
arnaudkrafft <arnaud.krafft@telecomnancy.net>
2026-02-16 14:10:02 +0100

    Added UI and enemy death logic

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -5,12 +5,35 @@ public class HealthScript : MonoBehaviour
 
     public float HP;
 
+    public float MaxHealth;
+
+    private void Start()
+    {
+        HP = MaxHealth;
+    }
     public void TakeDamage(float damage)
     {
         HP -= damage;
         if (GetComponent<EnemyNavigation>() != null)
         {
             GetComponent<EnemyNavigation>().engagedPlayer = true;
+
+            if (HP <= 0 && !GetComponent<EnemyNavigation>().ded)
+            {
+                GetComponent<EnemyNavigation>().PlayDeathAnim();
+            }
+            else
+            {
+                if (!GetComponent<EnemyNavigation>().Canvas.gameObject.activeSelf)
+                {
+                    GetComponent<EnemyNavigation>().Canvas.gameObject.SetActive(true);
+                }
+                GetComponent<EnemyNavigation>().Lifebar.fillAmount = HP / MaxHealth;
+            }
+        }
+        if (GetComponent<MovementController>() != null)
+        {
+            GetComponent<MovementController>().HPTMP.text = "HP : " + HP;
         }
     }
 }
